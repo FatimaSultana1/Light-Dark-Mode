678a995afb078c4f5ad7e71c5122ea9a80981e95
Fatima Sultana <fatima.shanaz30@gmail.com>
2022-04-04 20:28:15 +0530

    ImageMode function

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const image2=document.getElementById('image2');
 const image3=document.getElementById('image3');
 const textBox=document.getElementById('text-box');
 
+function imageMode(color){
+    image1.src=`img/undraw_proud_coder_${color}.svg`;
+    image2.src=`img/undraw_feeling_proud_${color}.svg`;
+    image3.src=`img/undraw_conceptual_idea_${color}.svg`;
+}
+
 //Dark mode styles
 function darkMode() {
     nav.style.backgroundColor='rgba(0 0 0 / 50%)';
@@ -13,9 +19,7 @@ function darkMode() {
     toggleIcon.children[0].textContent='Dark Mode';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
-    image1.src='img/undraw_proud_coder_dark.svg';
-    image2.src='img/undraw_feeling_proud_dark.svg';
-    image3.src='img/undraw_conceptual_idea_dark.svg';
+    imageMode('dark');
 }
 
 //Light mode styles
@@ -25,9 +29,7 @@ function lightMode(){
     toggleIcon.children[0].textContent='Light Mode';
     toggleIcon.children[1].classList.remove('fa-moon');
     toggleIcon.children[1].classList.add('fa-sun');
-    image1.src='img/undraw_proud_coder_light.svg';
-    image2.src='img/undraw_feeling_proud_light.svg';
-    image3.src='img/undraw_conceptual_idea_light.svg';
+    imageMode('light');
 }
 
 //Switch theme dynamically
